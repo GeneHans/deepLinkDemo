f0bae22db8fe176330a5eb0611b676174fd4f76f
gang.han <957334281@qq.com>
2019-05-11 00:55:35 +0800

    finish simple deeplink

---
+++ app/src/main/java/com/example/deeplinkdemo/DeepLinkActivity.kt
@@ -0,0 +1,39 @@
+package com.example.deeplinkdemo
+
+/**
+ *  @author Gene Hans
+ */
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class DeepLinkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deep_link)
+        handlerDeepLink()
+    }
+
+    private fun handlerDeepLink() {
+        var action = intent.action
+        var data = intent.data
+        if (data?.host != null) {
+            when (data.host) {
+                getString(R.string.deeplink_host) -> {
+                    toTargetPage()
+                }
+                else -> {
+                    finish()
+                }
+            }
+        } else
+            finish()
+    }
+
+    private fun toTargetPage() {
+        var intent = Intent()
+        intent.setClass(baseContext, TargetActivity::class.java)
+        startActivity(intent)
+    }
+}
